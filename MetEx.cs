b35e413a7fcc513b95cd83190fce70a1d6e686cb
RyanMakesMods <60587654+RyanMakesMods@users.noreply.github.com>
2020-10-20 13:08:06 -0500

    Removed 'PlaySoundItem' Command

--- MetEx.cs
+++ MetEx.cs
@@ -40,37 +40,4 @@ namespace MetEx
             Main.PlaySound(type, player.Center);
         }
     }
-    public class PlaySoundItem : ModCommand
-    {
-        public override CommandType Type
-            => CommandType.Chat;
-
-        public override string Command
-            => "playsounditem";
-
-        public override string Usage
-            => "/playsounditem <soundid>";
-
-        public override string Description
-            => "play a sound from an item";
-
-        public override void Action(CommandCaller caller, string input, string[] args)
-        {
-            Player player = Main.player[Main.myPlayer];
-            var soundType = args[0];
-            if (!int.TryParse(args[0], out int type))
-            {
-                if (type == 0)
-                {
-                    throw new UsageException($"{soundType} is not a valid SoundType");
-                }
-            }
-            int sound = 1;
-            if (args.Length >= 2)
-            {
-                sound = int.Parse(args[1]);
-            }
-            Main.PlaySound(SoundID.Item, player.Center);
-        }
-    }
 }
